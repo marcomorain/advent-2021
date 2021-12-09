3c0e2eae068d4a93f121786e0b125d2f2b94e42a
Marc O'Morain <marc@circleci.com>
2021-12-09 20:52:02 +0000

    One per file

--- src/previous.clj
+++ src/previous.clj
@@ -287,3 +287,85 @@
 
 (is (=  {:fuel 86397080, :dest 459}
          (search "input/day7" mean cost-accelerate)))
+
+
+
+;; Day 8
+
+;; Part 1 
+(defn parse-digits [line]
+  (let [digits (s/split line #"[\|\s]+")]
+    (take-last 4 digits)))
+
+(->> "input/day8"
+     (lines)
+     (mapcat parse-digits)
+     (map count)
+     (filter #{2 3 4 7})
+     (count))
+
+
+;; Part 2
+
+(defn parse-attempts [line]
+  (let [digits (s/split line #"[\|\s]+")]
+    (drop-last 4 digits)))
+
+(defn digit-with-length [n digits]
+  (->> digits
+       (map set)
+       (filter (comp (partial = n) count))
+       (set)))
+
+(def counts {0 6
+             1 2
+             2 5
+             3 5
+             4 4
+             5 5
+             6 6
+             7 3
+             8 7
+             9 6})
+
+(defn deduce-digits [line]
+  (let [digits (parse-attempts line)
+        options (into {} (for [i (range 10)]
+                           [i (digit-with-length (counts i) digits)]))
+        contains (fn [haystack needle]
+                   (filter #(set/superset? % needle) haystack))
+        one (first (options 1))
+        three (first (contains (options 3) one))
+        four (first (options 4))
+        seven (first (options 7))
+        eight (first (options 8))
+        be (set/difference eight three)
+        zero (first (contains (contains (options 6) one) be))
+        nine (first (remove #{zero} (contains (options 9) one)))
+        six (first (remove #{nine zero} (options 6)))
+        two-five (group-by #(empty? (set/difference % nine))
+                           (remove #{three} (options 2)))
+        two (first (two-five false))
+        five (first (two-five true))]
+    {zero 0
+     one 1
+     two 2
+     three 3
+     four 4
+     five 5
+     six 6
+     seven 7
+     eight 8
+     nine 9}))
+
+(defn compute [line]
+  (let [digits (deduce-digits line)
+        display (map set (parse-digits line))
+        nums (for [d display]
+               (get digits d))]
+    (parse-long (apply str nums))))
+
+(->> "input/day8"
+     (lines)
+     (map compute)
+     (reduce +))
